11abb4151ad6437038b57279d5231fc1685276f0
Jeffrey Biles <bilesjeffrey@gmail.com>
2023-09-17 15:19:23 -0500

    basic card

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css'
 
 function App() {
-
+  const title = 'Do Market Research'
+  const id = "BUS-1"
+  const points = 5
   return (
-    <div className="text-4xl text-red-500">
-      Hello
+    <div className="border rounded-lg px-2 m-2 bg-gray-50">
+      <div className="text-base font-semibold py-2">
+        {title}
+      </div>
+      <div className="flex gap-4 justify-between py-2 text-gray-700">
+        <div>{id}</div>
+        <div>{points}</div>
+      </div>
     </div>
   )
 }
