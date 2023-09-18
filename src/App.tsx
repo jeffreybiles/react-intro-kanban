b4e47e99efd461d753fde3c5be81218b3d43f1c3
Jeffrey Biles <bilesjeffrey@gmail.com>
2023-09-17 22:14:58 -0500

    arrays

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
 import './App.css'
 import TaskCard from './components/TaskCard'
-import { Task } from './utils/data-tasks'
+import { tasks } from './utils/data-tasks'
 
 function App() {
-  const task: Task = {
-    title: 'Do Market Research',
-    id: 'BUS-1',
-    points: 5,
-  }
-
   return (
     <>
-      <TaskCard task={task} />
-      {/* <TaskCard title='Competitor analysis' id='BUS-2' />
-      <TaskCard title='Develop Business Strategy' id='BUS-3' points={8} />
-      <TaskCard title='Develop Marketing Strategy' id='BUS-4' points={5} /> */}
+      {tasks.map((task) => <TaskCard task={task} />)}
     </>
   )
 }
--- src/utils/data-tasks.ts
+++ src/utils/data-tasks.ts
@@ -3,3 +3,25 @@ export type Task = {
   id: string,
   points?: number
 }
+
+export const tasks: Array<Task> = [
+  {
+    title: 'Do Market Research',
+    id: 'BUS-1',
+    points: 5,
+  },
+  {
+    title: 'Competitor analysis',
+    id: 'BUS-2',
+  },
+  {
+    title: 'Develop Business Strategy',
+    id: 'BUS-3',
+    points: 8,
+  },
+  {
+    title: 'Develop Marketing Strategy',
+    id: 'BUS-4',
+    points: 5,
+  }
+]
