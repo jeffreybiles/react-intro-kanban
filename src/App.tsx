94db0d6caad6e7ad04f3cfe4e63d830c2cc5d51b
Jeffrey Biles <bilesjeffrey@gmail.com>
2023-09-18 01:44:15 -0500

    update task points

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { useState } from 'react'
 import './App.css'
 import TaskCard from './components/TaskCard'
-import { tasks, statuses } from './utils/data-tasks'
+import { tasks as initialTasks, statuses, Task } from './utils/data-tasks'
 
 function App() {
+  const [tasks, setTasks] = useState<Task[]>(initialTasks)
   const columns = statuses.map((status) => {
     const tasksInColumn = tasks.filter((task) => task.status === status)
     return {
@@ -11,12 +13,27 @@ function App() {
     }
   })
 
+  const updateTaskPoints = (task: Task, points: number) => {
+    const updatedTasks = tasks.map((t) => {
+      return t.id === task.id ? { ...t, points } : t
+    })
+    setTasks(updatedTasks)
+  }
+
   return (
     <div className="flex divide-x">
       {columns.map((column) => (
         <div>
-          <h2 className="text-3xl p-2 capitalize font-bold text-gray-500">{column.title}</h2>
-          {column.tasks.map((task) => <TaskCard task={task} />)}
+          <div className="flex justify-between text-3xl p-2 font-bold text-gray-500">
+            <h2 className="capitalize">{column.title}</h2>
+            {column.tasks.reduce((total, task) => total + (task?.points || 0), 0)}
+          </div>
+          {column.tasks.map((task) => (
+            <TaskCard
+              task={task}
+              updateTaskPoints={updateTaskPoints}
+            />
+          ))}
         </div>
       ))}
     </div>
--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Task } from '../utils/data-tasks'
 
 const lowPriorityIcon = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -11,9 +12,11 @@ const highPriorityIcon = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 
   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 15l7-7 7 7" />
 </svg>
 
-const TaskCard = ({task}: {
+const TaskCard = ({task, updateTaskPoints}: {
   task: Task
+  updateTaskPoints: (task: Task, points: number) => void
 }) => {
+  const points = task.points || 0
   return <div className="border rounded-lg px-2 m-2 bg-gray-50 w-56">
     <div className="text-base font-base py-2">
       {task.title}
@@ -25,7 +28,11 @@ const TaskCard = ({task}: {
         {task.priority === 'medium' && mediumPriorityIcon}
         {task.priority === 'low' && lowPriorityIcon}
       </div>
-      <div>{task.points}</div>
+      <div className="flex gap-2 items-center">
+        <button onClick={() => updateTaskPoints(task, points - 1)}>-</button>
+        <div className="font-bold">{points}</div>
+        <button onClick={() => updateTaskPoints(task, points + 1)}>+</button>
+      </div>
     </div>
   </div>
 }
