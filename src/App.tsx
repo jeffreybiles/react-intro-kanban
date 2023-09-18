1be560c130ba1247ceb1ab8dd627558390ad4626
Jeffrey Biles <bilesjeffrey@gmail.com>
2023-09-18 04:51:18 -0500

    hover effects

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   const columns = statuses.map((status) => {
     const tasksInColumn = tasks.filter((task) => task.status === status)
     return {
-      title: status,
+      status,
       tasks: tasksInColumn
     }
   })
@@ -22,6 +22,7 @@ function App() {
 
   const handleDrop = (e: React.DragEvent<HTMLDivElement>, status: Status) => {
     e.preventDefault()
+    setCurrentlyHoveringOver(null)
     const id = e.dataTransfer.getData("id")
     const task = tasks.find((task) => task.id === id)
     if(task) {
@@ -29,20 +30,31 @@ function App() {
     }
   }
 
+  const [currentlyHoveringOver, setCurrentlyHoveringOver] = useState<Status | null>(null)
+  const handleDragEnter = (status: Status) => {
+    setCurrentlyHoveringOver(status)
+  }
+
   return (
     <div className="flex divide-x">
       {columns.map((column) => (
-        <div onDrop={(e) => handleDrop(e, column.title)} onDragOver={(e) => e.preventDefault()}>
+        <div
+          onDrop={(e) => handleDrop(e, column.status)}
+          onDragOver={(e) => e.preventDefault()}
+          onDragEnter={() => handleDragEnter(column.status)}
+        >
           <div className="flex justify-between text-3xl p-2 font-bold text-gray-500">
-            <h2 className="capitalize">{column.title}</h2>
+            <h2 className="capitalize">{column.status}</h2>
             {column.tasks.reduce((total, task) => total + (task?.points || 0), 0)}
           </div>
+          <div className={`h-full ${currentlyHoveringOver === column.status ? 'bg-gray-200' : ''}`}>
           {column.tasks.map((task) => (
             <TaskCard
               task={task}
               updateTask={updateTask}
             />
           ))}
+          </div>
         </div>
       ))}
     </div>
