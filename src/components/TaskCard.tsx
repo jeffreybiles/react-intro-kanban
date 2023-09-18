b311c81bdead33e95333de0b8ca5c01029befab6
Jeffrey Biles <bilesjeffrey@gmail.com>
2023-09-18 02:16:53 -0500

    fibbonaci sequence for points

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { Task } from '../utils/data-tasks'
 
 const lowPriorityIcon = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -17,6 +16,16 @@ const TaskCard = ({task, updateTaskPoints}: {
   updateTaskPoints: (task: Task, points: number) => void
 }) => {
   const points = task.points || 0
+  const updatePoints = (direction: 'up' | 'down') => {
+    const fib = [0, 1, 2, 3, 5, 8, 13]
+    const index = fib.indexOf(points)
+    const nextIndex = direction === 'up' ? index + 1 : index - 1
+    const newPoints = fib[nextIndex]
+    if(newPoints) {
+      updateTaskPoints(task, newPoints)
+    }
+
+  }
   return <div className="border rounded-lg px-2 m-2 bg-gray-50 w-56">
     <div className="text-base font-base py-2">
       {task.title}
@@ -29,9 +38,9 @@ const TaskCard = ({task, updateTaskPoints}: {
         {task.priority === 'low' && lowPriorityIcon}
       </div>
       <div className="flex gap-2 items-center">
-        <button onClick={() => updateTaskPoints(task, points - 1)}>-</button>
+        <button onClick={() => updatePoints('down')}>-</button>
         <div className="font-bold">{points}</div>
-        <button onClick={() => updateTaskPoints(task, points + 1)}>+</button>
+        <button onClick={() => updatePoints('up')}>+</button>
       </div>
     </div>
   </div>
