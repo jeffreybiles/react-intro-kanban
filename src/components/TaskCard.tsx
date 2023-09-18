77eaf66c36c6099e7b5547351b8c3c45183f1cc6
Jeffrey Biles <bilesjeffrey@gmail.com>
2023-09-17 23:50:14 -0500

    priority icons

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,5 +1,16 @@
 import { Task } from '../utils/data-tasks'
 
+const lowPriorityIcon = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-blue-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 9l7 7 7-7" />
+</svg>
+const mediumPriorityIcon = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-yellow-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 10h14" />
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 14h14" />
+</svg>
+const highPriorityIcon = <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 15l7-7 7 7" />
+</svg>
+
 const TaskCard = ({task}: {
   task: Task
 }) => {
@@ -8,7 +19,12 @@ const TaskCard = ({task}: {
       {task.title}
     </div>
     <div className="flex gap-4 justify-between py-2 text-gray-500 text-sm">
-      <div>{task.id}</div>
+      <div className="flex gap-2">
+        <div>{task.id}</div>
+        {task.priority === 'high' && highPriorityIcon}
+        {task.priority === 'medium' && mediumPriorityIcon}
+        {task.priority === 'low' && lowPriorityIcon}
+      </div>
       <div>{task.points}</div>
     </div>
   </div>
